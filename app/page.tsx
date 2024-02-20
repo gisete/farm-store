d142330164fc48d68a36fa2d2920babcd5062526
Gisete Kindahl <gisete@buffer.com>
2024-02-19 21:13:43 -0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,10 @@
 import Image from "next/image";
 import Products from "./(shop)/components/Products";
-import Cart from "./(shop)/components/Cart";
-import { getProducts } from "@lib/firebase";
-
-// export async function getServerSideProps() {
-// 	const products = await getProducts();
-
-// 	return {
-// 		props: {
-// 			products,
-// 		},
-// 	};
-// }
-async function getMyProducts() {
-	const res = await getProducts();
-	const products = await res.json();
-
-	return products;
-}
 
 export default function Home() {
 	return (
-		<div className="flex flex-col items-center justify-between p-10">
-			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-				<h2>Week of June 30th</h2>
-			</div>
-
-			<Products />
+		<div className="flex flex-col w-full h-full px-4 py-12 overflow-y-auto">
+			<Image src="/img/hdpd-logo.png" alt="Logo" width="100" height="100" className="self-center" />
 		</div>
 	);
 }
