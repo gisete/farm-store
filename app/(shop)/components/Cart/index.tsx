ebbb77b679298d35cf67b1cfaac082f0f9b09b4b
Gisete Kindahl <gisete@buffer.com>
2024-04-25 17:04:44 +0100

    changes

--- app/(shop)/components/Cart/index.tsx
+++ app/(shop)/components/Cart/index.tsx
@@ -35,12 +35,30 @@ const Cart = ({ hideButton }: CartProps) => {
 	return (
 		<>
 			<div
-				className={`bg-white w-full fixed md:w-100 md:static top-0 bottom-0 right-0 translate-x-full md:transform-none cart-closed ${
+				className={`bg-white w-full fixed md:w-100 md:static top-0 bottom-0 right-0 translate-x-full md:transform-none cart-closed z-10 ${
 					isCartOpen ? "cart-open" : ""
 				}`}
 			>
-				<div className="p-6 flex flex-col">
+				<div className="pt-16 md:pt-6 p-6 flex flex-col">
 					<h2 className="text-center text-lg mb-6">Cabaz</h2>
+
+					<button
+						type="button"
+						className="bg-white absolute top-4 right-4 p-4 inline-flex items-center justify-center text-black focus:outline-none focus:ring-2 focus:ring-inset w-[55px] h-[55px] rounded-full border border-black"
+						onClick={() => setCartOpen(false)}
+					>
+						<span className="sr-only">Close menu</span>
+						<svg
+							className="h-6 w-6"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+						</svg>
+					</button>
+
 					<ul>
 						{cart.map((item, index) => (
 							<CartItem item={item} key={`cartitem-${index}`} />
@@ -86,9 +104,9 @@ const Cart = ({ hideButton }: CartProps) => {
 				</div>
 			</div>
 			<button
-				className="fixed top-4 right-0 bg-orange-500 text-white active:bg-red-600 px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+				className="fixed w-20 h-20 top-4 right-0 bg-orange-300 text-white active:bg-orange-500 px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
 				type="button"
-				onClick={() => setCartOpen(!isCartOpen)}
+				onClick={() => setCartOpen(true)}
 			>
 				<Image src="/img/icon-basket.png" width={40} height={40} alt="Picture of the author" />
 			</button>
